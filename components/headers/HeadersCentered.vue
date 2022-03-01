8a4c1cc6cab8b2cbb1bd9ea113e7b21a23cb78cd
Alvaro Gomez Traveso <agtraveso@gmail.com>
2022-03-01 13:30:42 +0100

    Fix email input in header for mobile

--- components/headers/HeadersCentered.vue
+++ components/headers/HeadersCentered.vue
@@ -18,19 +18,19 @@
         that lets you build your landing page without effort.
       </p>
 
-      <div class="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm">
-        <div>
+      <div class="mt-6 justify-center sm:flex sm:items-center">
+        <div class="w-full sm:max-w-xs">
           <input
             id="email"
             type="email"
             name="email"
-            class="highlight-white sm:flex items-center w-72 text-left space-x-3 px-4 h-12 bg-white shadow-md dark:shadow-white border-0 hover:ring-400 hover:ring-slate-300 focus:ring-0 focus:outline-none focus:border-slate-500 rounded-lg text-slate-400 dark:bg-slate-800 dark:text-slate-300 dark:hover:bg-slate-700"
+            class="highlight-white sm:flex items-center w-full text-left px-4 h-12 bg-white shadow-md dark:shadow-white border-0 hover:ring-400 hover:ring-slate-300 focus:ring-0 focus:outline-none focus:border-slate-500 rounded-lg text-slate-400 dark:bg-slate-800 dark:text-slate-300 dark:hover:bg-slate-700"
             placeholder="Enter your email"
           />
         </div>
         <button
           type="button"
-          class="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-indigo-500 dark:shadow-sm dark:hover:bg-indigo-400"
+          class="mt-3 sm:mt-0 sm:ml-3 bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-indigo-500 dark:shadow-sm dark:hover:bg-indigo-400"
         >
           Notify me
         </button>
